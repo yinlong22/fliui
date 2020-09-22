27823e980f4d3a933cbd52348443488be4e47aed
ylong <342241857@qq.com>
2020-09-22 16:38:52 +0800

    完成Dialog组件 并用teleport将组件插到body下

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,15 +1,38 @@
 <template>
     <h1>Dialog 示例</h1>
     <div>示例1</div>
-    <Dialog></Dialog>
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :close-on-click-overlay="false" :ok="fn">
+        <template v-slot:title>
+            <h2>tip</h2>
+        </template>
+        <template v-slot:content>
+            <p>content one</p>
+            <p>content two</p>
+            <p>content three</p>
+        </template>
+    </Dialog>
 </template>
 
 <script lang="ts">
     import Dialog from '../lib/Dialog.vue'
+    import Button from '../lib/Button.vue'
+    import {ref} from 'vue'
 
     export default {
         components: {
+            Button,
             Dialog
+        },
+        setup() {
+            const x = ref(false)
+            const toggle = () => {
+                x.value = !x.value
+            }
+            const fn = () => {
+                return false
+            }
+            return {x, toggle, fn}
         }
     }
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,24 +1,112 @@
 <template>
-    <div class="fli-dialog-overlay"></div>
-    <div class="fli-dialog-overlay">
-        <header>标题</header>
-        <main>
-            <p>content1</p>
-            <p>content2</p>
-        </main>
-        <footer>
-            <Button>Ok</Button><Button>Cancel</Button>
-        </footer>
+    <div v-if="visible">
+        <Teleport to="body">
+            <div class="fli-dialog-overlay" @click="onclickOverlay"></div>
+            <div class="fli-dialog-wrapper">
+                <div class="fil-dialog">
+                    <header>
+                        <slot name="title"/>
+                        <img src="../assets/cancel.svg" alt="" @click="close">
+                    </header>
+                    <hr>
+                    <main>
+                        <slot name="content"/>
+                    </main>
+                    <hr>
+                    <footer>
+                        <Button background="red" @click="ok">Ok</Button>
+                        <Button background="black" @click="close">Cancel</Button>
+                    </footer>
+                </div>
+            </div>
+        </Teleport>
     </div>
 </template>
 
 <script lang="ts">
     import Button from './Button.vue'
+
     export default {
-        components: {Button}
+        props: {
+            visible: {
+                type: Boolean,
+                default: false
+            },
+            closeOnClickOverlay: {
+                type: Boolean,
+                default: true
+            },
+            ok: {
+                type: Function
+            }
+        },
+        components: {Button},
+        setup(props, context) {
+            const close = () => {
+                context.emit('update:visible', false)
+            }
+            const onclickOverlay = () => {
+                if (props.closeOnClickOverlay) {
+                    close()
+                }
+            }
+            const ok = () => {
+                if (props.ok?.() !== false) {
+                    close()
+                }
+            }
+            return {close, onclickOverlay, ok}
+        }
     }
 </script>
 
 <style lang="scss">
+    $radius: 4px;
+    $border-color: #d9d9d9;
+    .fli-dialog {
+        &-overlay {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: rgba(0, 0, 0, .4);
+            z-index: 11;
+        }
+        &-wrapper {
+            background: white;
+            color: #666666;
+            border-radius: $radius;
+            box-shadow: 0 0 3px fade_out(black, 0.5);
+            min-width: 15em;
+            max-width: 90%;
+            width: 334px;
+            border:1px solid;
+            height: auto;
+            position: absolute;
+            left: 6%;
+            top: 25%;
+            z-index: 15;
+            header{
+                display: flex;
+                margin: 7px;
+                justify-content: space-between;
+                img{
+                    width: 16px;
+                    height: 33px;
+                }
+            }
+            hr{
+                margin-bottom: 10px;
+            }
+            main{
+                margin-left: 10px;
+            }
+            footer{
+                margin-left: 202px;
+                margin-bottom: 10px;
+            }
+        }
+    }
 
 </style>
