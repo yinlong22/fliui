5241cd613bffd785cccb8c229e321ff41ea29ae3
ylong <342241857@qq.com>
2020-09-25 14:52:56 +0800

    完成switch样式优化及代码展示

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,25 +1,70 @@
 <template>
     <div>
         <h1>switch示例</h1>
-        <div>
-            <Switch v-model:value="bool"/>
+        <div class="fli-addCode"><span>颜色</span>
+            <Button @click="codeVisible" background="black">代码</Button>
         </div>
-    </div>
+        <div class="fli-showCode">
+            <Switch v-model:value="bool"/>
+            <Switch theme="red" v-model:value="bool2"/>
+            <Switch theme="blueL" v-model:value="bool3"/>
+            <span></span><span></span><span></span>
+        </div>
+        <code ref="className">{{code}}</code>
+        </div>
 </template>
 
 <script lang="ts">
     import Switch from '../lib/Switch.vue'
     import {ref} from 'vue'
+    import Button from '../lib/Button.vue'
 
     export default {
-        components: {Switch},
+        components: {Button, Switch},
         setup() {
             const bool = ref(true)
-            return {bool}
+            const bool2 = ref(true)
+            const bool3 = ref(true)
+            const code =
+`<template>
+<Switch v-model:value="bool"/>
+<Switch theme="red" v-model:value="bool2"/>
+<Switch theme="blueL" v-model:value="bool3"/>
+</template>
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+import {ref} from 'vue'
+export default {
+   components: {Switch},
+      setup() {
+      //每个Switch的状态值是$bool 默认选中为true
+         const bool = ref(true)
+         const bool2 = ref(true)
+         const bool3 = ref(true)
+   }
+   return {bool, bool2, bool3}
+}
+`
+            const className = ref<HTMLDivElement>(null)
+            const codeVisible = () => {
+                const classes = className.value.classList
+                !classes.contains('show') ? classes.add('show') : classes.remove('show')
+            }
+            return {bool, bool2, bool3, code, className, codeVisible}
         }
     }
 </script>
 
 <style lang="scss" scoped>
-
+    code {
+        border-radius: 3px;
+        opacity: 0;
+        visibility: hidden;
+        &.show {
+            opacity: 1;
+            visibility: visible;
+            display: block;
+        }
+        transition:opacity 1s;
+    }
 </style>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,20 +1,33 @@
 <template>
     <div>
-        <button @click="toggle" class="fli-switch" :class="{'fli-clicked':value}"><span></span></button>
+        <button @click="toggle" class="fli-switch" :class="classes"><span></span></button>
     </div>
 </template>
 
 <script lang="ts">
 
+    import {computed} from 'vue'
+
     export default {
         props: {
-            value: Boolean //对应emit暴露的事件
+            value: Boolean, //对应emit暴露的事件
+            theme: {
+                type: String,
+                default: ''
+            }
         },
         setup(props, context) {
             const toggle = () => {
                 context.emit('update:value', !props.value)
             }
-            return {toggle}
+            const {theme} = props
+            const classes = computed(() => {
+                return {
+                    [`fli-clicked`]: props.value,
+                    [`fli-clicked-${theme}`]: props.theme,
+                }
+            })
+            return {toggle, classes}
         }
     }
 </script>
@@ -22,7 +35,9 @@
 <style lang="scss">
     $h: 22px;
     $h2: $h - 4px;
-    $color: #F7515B;
+    $color: #4CA0FF;
+    $color2: #F7515B;
+    $color3: #40a99f;
     .fli-switch {
         height: $h;
         width: $h*2.1;
@@ -41,14 +56,24 @@
             border-radius: $h2/2;
             transition: all 250ms;
         }
+
+
         &.fli-clicked {
             background: $color;
 
             > span {
                 left: calc(100% - #{$h2} - 2px);
             }
-        }
 
+
+
+        }
+        &.fli-clicked.fli-clicked-red {
+            background: $color2;
+        }
+        &.fli-clicked.fli-clicked-blueL {
+            background: $color3;
+        }
         &:focus {
             outline: none;
         }
@@ -66,4 +91,5 @@
             }
         }
     }
+
 </style>
